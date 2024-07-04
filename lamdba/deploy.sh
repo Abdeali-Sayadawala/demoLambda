0d0d26dfb1278fe94be0d645cae561a99a02e954
Abdeali Sayadawala <abdeali009@gmail.com>
2024-07-04 21:54:16 +0530

    addded lambda layers

--- lamdba/deploy.sh
+++ lamdba/deploy.sh
@@ -8,8 +8,10 @@ for dir in ./*; do
 
     latest_layer_arn=$(aws lambda list-layer-versions --layer-name $layer_name --query 'LayerVersions[0].LayerVersionArn')
     latest_layer_arn="${latest_layer_arn//\"/}"
-    layer_url=$(aws lambda get-layer-version-by-arn --arn $latest_layer_arn --query 'Content.CodeSha256')
-    echo "Layer sha code: $layer_url"
+    layer_sha=$(aws lambda get-layer-version-by-arn --arn $latest_layer_arn --query 'Content.CodeSha256')
+    current_sha=$(sha256sum $layer_name.zip)
+    echo "Layer sha code: $layer_sha"
+
     echo "creating lambda layer: $layer_name"
     layer_arn=$(aws lambda publish-layer-version --layer-name $layer_name --zip-file fileb://$layer_name.zip --compatible-runtimes python3.10 python3.11 python3.12 | python -c 'import json,sys;obj=json.load(sys.stdin);print(obj["LayerVersionArn"])')
     echo "Layer version ARN: $layer_arn"
