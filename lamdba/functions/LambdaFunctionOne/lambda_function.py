6c4371a5b809474daad2cb1e3c8ee081e68d6718
Abdeali Sayadawala <abdeali009@gmail.com>
2024-07-08 17:41:47 +0530

    changes in lambda deploy

--- lamdba/functions/LambdaFunctionOne/lambda_function.py
+++ lamdba/functions/LambdaFunctionOne/lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda function one!')
+        'body': json.dumps('Hello from Lambda function one! This is the updated function')
     }
